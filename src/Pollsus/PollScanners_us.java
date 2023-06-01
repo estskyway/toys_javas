3c0d85ea5bf3aac61a252572608d267b499d9068
estskyway <estskyway@gmail.com>
2023-06-01 15:06:16 +0900

    6/1 15시 6분

--- src/Pollsus/PollScanners_us.java
+++ src/Pollsus/PollScanners_us.java
@@ -1,5 +1,27 @@
 package Pollsus;
 
+import java.util.Scanner;
+
 public class PollScanners_us {
-    public 
+    public String[] PollsWithAnswers(String[][] polls) {
+       String[] answers = {"", ""};
+    try {
+        int count = 0;
+        Scanner myObj = new Scanner(System.in);  
+        for (int second=0; second < polls.length; second=second+2){
+            System.out.println(polls[second][0]);
+            // 답항 출력 본인이 해 보기
+
+            System.out.print("답하기 : ");
+            answers[count] = myObj.nextLine();
+            count = count + 1;
+            System.out.println();
+        }
+        myObj.close();
+       } catch (Exception e) {
+        // TODO: handle exception
+       }
+       return answers;
+    
+}
 }
