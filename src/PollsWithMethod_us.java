7ce34f7c7b742ddee17e64d990806858ad8f7792
estskyway <estskyway@gmail.com>
2023-06-01 15:31:36 +0900

    6/1 15시 31분

--- src/PollsWithMethod_us.java
+++ src/PollsWithMethod_us.java
@@ -5,10 +5,15 @@ import Pollsus.PollStatistics_us;
 public class PollsWithMethod_us {
     public static void main(String[] args) {
         try {
-            PollInitailArrays_us pollInitailArrays_us = new PollInitailArrays_us();
+            String[] answers;
+            String[][] polls;
+
+            PollInitialArrays_us pollInitailArrays_us = new PollInitialArrays_us();
             polls = pollInitailArrays_us.init();    //설문 내용 초기화
-            new PollScanners_us();
-            new PollStatistics_us();
+            PollScanners_us pollScanners_us = new PollScanners_us();
+            answers = pollScanners_us.PollsWithAnswers(polls);
+            PollStatistics_us pollStatistics_us = new PollStatistics_us();
+            pollStatistics_us.printAnswers(answers);
         } catch (Exception e) {
             // TODO: handle exception
         }
--- src/Pollsus/PollStatistics_us.java
+++ src/Pollsus/PollStatistics_us.java
@@ -1,5 +1,14 @@
 package Pollsus;
 
 public class PollStatistics_us {
-    
+    public int printAnswers(String[] answers){
+        try {
+            for (int first=0; first < answers.length; first=first+1) {
+                System.out.print(answers[first]+", ");
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return 1;
+    }
 }
